46592a192117f918b3f69129573b63fc5624e0a1
holdenmc <holdenjmcginnis@gmail.com>
2017-07-16 10:20:45 -0400

    React hello world

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-document.write("Hello World!");
+"use strict";
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const styles = {
+  app: {
+    paddingTop: 40,
+    textAlign: 'center'
+  }
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div style={styles.app}>
+        Welcome To React!
+      </div>
+    )
+  }
+}
+
+const root = document.querySelector('#app-west');
+ReactDOM.render(<App />, root);
