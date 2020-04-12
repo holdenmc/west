b344cf386320e4a09899039fde33880562c3f3ce
Holden McGinnis <holdenmcginnis@Holdens-MacBook-Pro.local>
2020-04-12 18:52:10 -0400

    add src folder with enums and update deck to match them

---
+++ src/enums/rank.ts
@@ -0,0 +1,70 @@
+export enum Rank {
+    TWO = 2,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+}
+
+export const en = {
+    TWO: {
+        ordinal: Rank.TWO,
+        display: '2',
+    },
+    THREE: {
+        ordinal: Rank.THREE,
+        display: '3',
+    },
+    FOUR: {
+        ordinal: Rank.FOUR,
+        display: '4',
+    },
+    FIVE: {
+        ordinal: Rank.FIVE,
+        display: '5',
+    },
+    SIX: {
+        ordinal: Rank.SIX,
+        display: '6',
+    },
+    SEVEN: {
+        ordinal: Rank.SEVEN,
+        display: '7',
+    },
+    EIGHT: {
+        ordinal: Rank.EIGHT,
+        display: '8',
+    },
+    NINE: {
+        ordinal: Rank.NINE,
+        display: '9',
+    },
+    TEN: {
+        ordinal: Rank.TEN,
+        display: '10',
+    },
+    JACK: {
+        ordinal: Rank.JACK,
+        display: 'J',
+    },
+    QUEEN: {
+        ordinal: Rank.QUEEN,
+        display: 'Q',
+    },
+    KING: {
+        ordinal: Rank.KING,
+        display: 'K',
+    },
+    ACE: {
+        ordinal: Rank.ACE,
+        display: 'A',
+    },
+};
---
+++ src/enums/suit.ts
@@ -0,0 +1,25 @@
+export enum SUIT {
+    CLUB,
+    DIAMOND,
+    HEART,
+    SPADE,
+}
+
+export const en = {
+    CLUB: {
+        ordinal: SUIT.CLUB,
+        display: 'Club'
+    },
+    DIAMOND: {
+        ordinal: SUIT.DIAMOND,
+        display: 'Diamond'
+    },
+    HEART: {
+        ordinal: SUIT.HEART,
+        display: 'Heart'
+    },
+    SPADE: {
+        ordinal: SUIT.SPADE,
+        display: 'SPADE'
+    },
+};
