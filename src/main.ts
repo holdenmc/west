43f7b26cf41dee40cc679fdf351e2ec8459589df
Holden McGinnis <holdenjmcginnis@gmail.com>
2020-04-18 13:55:31 -0400

    add socket.io hello world

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { init } from './connection';
 import express from 'express';
+import http from 'http';
+import socket from 'socket.io';
+
+import { init } from './connection';
 import { register as registerUserRoutes } from './routes/user';
 import { register as registerGameRoutes } from './routes/game';
 
@@ -9,10 +12,24 @@ init();
 // Initialize express server
 const app = express();
 const port = 3000;
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+const server = http.createServer(app);
+const io = socket(server);
 
 registerUserRoutes(app);
 registerGameRoutes(app);
-app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/index.html');
+});
+// TODO: Once there are static assets
+// app.use('/client', express.static(__dirname + '/client/static'));
+
+server.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
 
